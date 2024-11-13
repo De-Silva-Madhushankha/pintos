ab427a329fc09305b0f6eb95b9f544700777755d
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-11-13 21:41:38 +0530

    minor changes

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,5 +1,18 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include "threads/synch.h"
+#include "filesys/file.h"
+
+/* Structures for file system lock and file descriptor. */
+struct lock file_system_lock;       
+
+struct file_descriptor
+{
+    struct file *file;
+    int fd;                         
+    struct list_elem fd_elem;
+};
+
 
 void syscall_init (void);
 
